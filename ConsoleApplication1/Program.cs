65f09e3a6fe51d00fb7e85b0382e1d46195e41a5
athikumar72 <athikumar72@gmail.com>
2016-08-31 05:40:29 -0400

    Factorial using Recusion

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FactorialCalculation;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            
+
+        {
+
+            string option = "";
+
+            do
+            {
+               
+                Console.WriteLine("Welcome to the Factorial Calculation");
+                Console.WriteLine("Enter an integer that's  greater than 0 but less  than 10:");
+                int fact_input = int.Parse(Console.ReadLine());
+
+
+                Class1 fact1 = new Class1();
+                Console.WriteLine("The factorial of   " + fact_input  +"  is  " + fact1.factCalculator(fact_input) );
+
+                Console.WriteLine("y/n");
+                option = Console.ReadLine();
+            } while (option == "y");
+
+            
+        }
+    }
+}
---
+++ FactorialCalculation/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FactorialCalculation
+{
+    public class Class1
+    {
+        // int fact;
+     //  int value = 1;
+       // int i = 1;
+
+        public int factCalculator(int fact_meth)
+        {
+            //   int value = 1;
+            //int i = 1;
+
+            // i  = i+1;
+
+            /* if (fact1 < 0 || fact1 > 10)
+
+             {
+                 // (i <= fact1)
+                 Console.WriteLine(" The entered number is outof range enter another number");
+                 return fact1 = int.Parse(Console.ReadLine());
+
+
+             }
+             else */
+            if (fact_meth == 1)
+            {
+                return 1;
+                //return value;
+            }
+            else
+            {
+                return fact_meth * factCalculator(--fact_meth);
+
+
+
+            }
+            
+
+
+            }
+           
+
+          
+
+        }   
+    /*   int value = 1;
+           
+            for(int i=1;i<= fact1;i++)
+            {
+               value = value * i;
+                //Console.WriteLine("The factorial of   " + value + "  is   " + value);
+
+            }
+            return value;*/
+    }
+
+
